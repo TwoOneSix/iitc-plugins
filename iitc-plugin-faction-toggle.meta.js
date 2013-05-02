aa2d0322786ea2d2b6fb361c1d686f0d8a09a058
TwoOneSix <josh.zeno@gmail.com>
2013-05-02 00:22:19 -0300

    Initial Release

--- iitc-plugin-faction-toggle.meta.js
+++ iitc-plugin-faction-toggle.meta.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @id             iitc-plugin-faction-toggle@TwoOneSix
 // @name           IITC plugin: Limit viewable portals by specific faction
-// @version        0.0.1.20130427.095510
+// @version        0.0.1.20130501.112017
 // @namespace      https://github.com/TwoOneSix/iitc-plugins
 // @updateURL      https://raw.github.com/TwoOneSix/iitc-plugins/master/iitc-plugin-faction-toggle.meta.js
 // @downloadURL    https://raw.github.com/TwoOneSix/iitc-plugins/master/iitc-plugin-faction-toggle.user.js
-// @description    [2013-04-27-095510] Limit viewable portals by specific faction
+// @description    [2013-05-01-112017] Limit viewable portals by specific faction
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
